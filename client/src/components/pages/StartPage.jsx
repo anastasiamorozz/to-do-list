456df991a745d09eaf9e53b32072db7a09507981
Anastasia M <morozanastasia2005@gmail.com>
2023-12-27 23:51:47 +0200

    Improve stert page and make footer

--- client/src/components/pages/StartPage.jsx
+++ client/src/components/pages/StartPage.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import '../pages/StartPage.scss';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faNode, faJs, faSass, faNpm } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faNode, faJs, faSass, faNpm, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 const StartPage = () => {
@@ -14,7 +15,13 @@ const StartPage = () => {
                     <div className="bannerLeft">
                         <h1>Wellcome to best ToDoList!</h1>
                         <p>Create your own tasks and organize them by categories.</p>
-                        <button className='custom-btn btn-5'>Start it for free</button>
+                        <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Start it for free
+                        </a>
                     </div>
                     <img src='/cosmos.svg'></img>
                 </div>
@@ -22,15 +29,28 @@ const StartPage = () => {
                 <div className="stack">
                     {/* <h2>Technology stack</h2> */}
                     <ul>
-                        <li><FontAwesomeIcon icon={faReact} /></li>
-                        <li><FontAwesomeIcon icon={faNode} /></li>
-                        <li><FontAwesomeIcon icon={faJs} /></li>
-                        <li><FontAwesomeIcon icon={faSass} /></li>
-                        <li><FontAwesomeIcon icon={faNpm} /></li>
+                        <li className='logoReact'><FontAwesomeIcon icon={faReact} /></li>
+                        <li className='logoNode'><FontAwesomeIcon icon={faNode} /></li>
+                        <li className='logoJS'><FontAwesomeIcon icon={faJs} /></li>
+                        <li className='logoSass'><FontAwesomeIcon icon={faSass} /></li>
+                        <li className='logoNPM'><FontAwesomeIcon icon={faNpm} /></li>
                     </ul>
                 </div>
+
+                <div >
+                    <a className="codeLink" href='https://github.com/anastasiamorozz/to-do-list'> 
+                        <div className="leftSide">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </div>
+                        <div className="rightSide">
+                            <h1>This project is open source</h1>
+                            <p>You can find all code by one click</p>
+                        </div>
+                    </a>
+                </div>
+
             </div>
-            
+            <Footer></Footer>
         </div>
     );
 }
